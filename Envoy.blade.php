556da9d3c0b5cc5e2bb7e18c1e2f44b5a25a5a5b
Alex Drimbe <alex@fivetailors.com>
2017-04-16 20:50:07 +0300

    envoy

---
+++ Envoy.blade.php
@@ -0,0 +1,14 @@
+@servers(['web' => ['root@drimbe.info']])
+
+@task('deploy', ['on' => 'web'])
+	cd /var/www/html
+	
+	git pull origin master
+	
+	composer install
+	php artisan migrate --force
+	php artisan config:cache
+	php artisan route:cache
+	php artisan optimize
+	php artisan opcache:clear
+@endtask
